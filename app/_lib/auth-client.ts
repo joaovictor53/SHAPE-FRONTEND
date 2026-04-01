59b60b7c40e471267f6c158e3eb08a342b0113e0
joao53 <victors.rpe@gmail.com>
2026-04-01 08:35:29 -0400

    feat: initialize project with Tailwind CSS, shadcn/ui and components

---
+++ app/_lib/auth-client.ts
@@ -0,0 +1,10 @@
+import { createAuthClient } from "better-auth/client";
+
+export const authClient = createAuthClient({
+    baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL,
+    
+})
+
+authClient.signIn.social({
+    provider: "google",
+})
---
+++ app/auth/auth.tsx
@@ -0,0 +1,7 @@
+const AuthPage = () => {
+    return (
+        <div>
+            <h1>Auth</h1>
+        </div>
+    )
+}
